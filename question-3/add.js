2d83e97f5ffda2843aea08285918569f2bc1a075
Haley <hs.haleyk@gmail.com>
2024-10-03 17:48:19 -0400

    question 3 added

--- question-3/add.js
+++ question-3/add.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
 
@@ -6,10 +7,8 @@ const logsDir = path.join(__dirname, 'Logs');
 if (!fs.existsSync(logsDir))
     fs.mkdirSync(logsDir);
 
-process.chdir(logsDir);
-
 for (let i = 0; i < 10; i++) {
-    const fileName = `log${i}.txt`;
+    const fileName = path.join(logsDir, `log${i}.txt`);
     fs.writeFileSync(fileName, `This is log file ${i}`);
-    console.log(fileName);
+    console.log(`log${i}.txt`);
 }
